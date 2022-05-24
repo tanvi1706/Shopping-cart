6a6f8532decf720e8d71f859c08935ccaff9def9
Tanvi Patil <tanvipatil@Tanvis-MacBook-Pro.local>
2022-05-24 13:22:15 -0400

    cart ready

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import {  Fragment, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
+// import {uiActions} from './store/ui-slice';
+import Notification from './components/UI/Notification';
+import { sendCartData, fetchCartData } from './store/cart-actions';
+
+let initial = true;
+
+function App() {
+  const dispatch = useDispatch();
+  const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+  const notification = useSelector(state => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  },[dispatch]);
+
+  useEffect(() => {
+    // const sendCartData = async () =>{
+      // dispatch(uiActions.showNotification({
+      //   status: 'pending',
+      //   title: 'Sending....',
+      //   message: 'Sending cart data!',
+      // }));
+    //   const response = await fetch('https://mealsorder-777f8-default-rtdb.firebaseio.com/cart.json',{
+    //     method: 'PUT',
+    //     body: JSON.stringify(cart),
+    // });
+    // if(!response.ok){
+    // throw new Error('Sending cart data failed!');
+     
+    // }
+    
+    // dispatch(uiActions.showNotification({
+    //   status: 'success',
+    //   title: 'Success',
+    //   message: 'Sent cart data successfully!',
+    // }));
+  // };
+
+  // if (initial){
+  //   initial = false
+  //   return;
+  // }
+  // sendCartData().catch((err) => {
+    // dispatch(
+    //   uiActions.showNotification({
+    //   status: 'error',
+    //   title: 'error',
+    //   message: 'Sending cart data failed!',
+    // })
+    // );
+  // });
+
+    if(initial){
+      initial = false;
+      return;
+    }
+    if(cart.changed){
+      dispatch(sendCartData(cart));
+    }
+    
+  }, [cart, dispatch]);
+
+  return (
+    <Fragment>
+      {notification && (
+      <Notification 
+        status={notification.status} 
+        title={notification.title} 
+        message={notification.message}/>)}
+      <Layout>
+      {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Cart/CartButton.js
@@ -0,0 +1,23 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { uiActions } from '../../store/ui-slice';
+
+import classes from './CartButton.module.css';
+
+const CartButton = (props) => {
+
+  const quantity = useSelector(state => state.cart.totalQuantity)
+  const dispatch = useDispatch();
+  const toggleHandler = () => {
+    dispatch(uiActions.toogle());
+  };
+
+  return (
+    <button className={classes.button} onClick={toggleHandler}>
+      <span>My Cart</span>
+      <span className={classes.badge}>{quantity}</span>
+    </button>
+  );
+};
+
+export default CartButton;
